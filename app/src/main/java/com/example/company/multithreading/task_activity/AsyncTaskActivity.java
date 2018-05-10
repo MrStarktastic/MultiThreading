f6278db9d9da710aaba7e172bd2cb8437182b109
Ben Faingold <benfaingold@gmail.com>
2018-05-10 23:51:37 +0300

    Nothing

--- app/src/main/java/com/example/company/multithreading/task_activity/AsyncTaskActivity.java
+++ app/src/main/java/com/example/company/multithreading/task_activity/AsyncTaskActivity.java
@@ -3,7 +3,7 @@ package com.example.company.multithreading.task_activity;
 import com.example.company.multithreading.counter_task.CounterAsyncTask;
 import com.example.company.multithreading.counter_task.CounterTask;
 
-public class AsyncTaskActivity extends TaskActivity {
+public final class AsyncTaskActivity extends TaskActivity {
 
     @Override
     protected CounterTask getTask() {
--- app/src/main/java/com/example/company/multithreading/task_activity/ThreadsActivity.java
+++ app/src/main/java/com/example/company/multithreading/task_activity/ThreadsActivity.java
@@ -3,7 +3,7 @@ package com.example.company.multithreading.task_activity;
 import com.example.company.multithreading.counter_task.CounterThreadsTask;
 import com.example.company.multithreading.counter_task.CounterTask;
 
-public class ThreadsActivity extends TaskActivity {
+public final class ThreadsActivity extends TaskActivity {
 
     @Override
     protected CounterTask getTask() {
